b4f84574a1b8fc865b25ee574df761bc95de2db2
Oleg Steba <oleg.steba@gmail.com>
2024-04-03 12:37:29 +0500

    Fixed: include bootstrap

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
